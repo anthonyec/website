ab3c7cc6ab857f4e9659bd30037213a42fbf79b7
Anthony Cossins <anthonyecossins@gmail.com>
2021-11-09 10:57:55 +0000

    Add pages and copy assets

--- src/config.js
+++ src/config.js
@@ -1,12 +1,54 @@
+const fs = require('fs');
+const path = require('path');
+
 module.exports = {
-  async getPages({ collection }) {
+  async getPages({ collection  }) {
     const posts = await collection(
       'posts',
       'post',
       './src/_posts',
       '/posts/{{slug}}'
     );
+    const standalone = [
+      {
+        slug: '404.html',
+        path: '/',
+        content: fs.readFileSync('./src/404.html', 'utf8')
+      },
+      {
+        slug: 'wechat.html',
+        path: '/',
+        content: fs.readFileSync('./src/wechat.html', 'utf8')
+      },
+      {
+        slug: 'humans.txt',
+        path: '/',
+        content: fs.readFileSync('./src/humans.txt', 'utf8')
+      },
+      {
+        slug: 'index.html',
+        path: '/',
+        content: fs.readFileSync('./src/index.html', 'utf8')
+      }
+    ];
 
-    return [...posts]
+    return [...posts, ...standalone]
+  },
+  postBuild: async () => {
+    // TODO: Make this less hacky!
+    fs.cpSync(path.join('./src', 'assets'), path.join('./dist', 'assets'), {
+      recursive: true
+    });
+
+    fs.cpSync(path.join('./src', 'v'), path.join('./dist', 'v'), {
+      recursive: true
+    });
+
+    fs.cpSync(path.join('./src', 'google0290ed350f9faa96.html'), path.join('./dist', 'google0290ed350f9faa96.html'));
+    fs.cpSync(path.join('./src', 'cv_2019.pdf'), path.join('./dist', 'cv_2019.pdf'));
+    fs.cpSync(path.join('./src', 'cv_2021.pdf'), path.join('./dist', 'cv_2021.pdf'));
+    fs.cpSync(path.join('./src', 'cv.pdf'), path.join('./dist', 'cv.pdf'));
+    fs.cpSync(path.join('./src', 'favicon.png'), path.join('./dist', 'favicon.png'));
+    fs.cpSync(path.join('./src', 'favicon.ico'), path.join('./dist', 'favicon.ico'));
   }
 };
